2a06be844d9e6d4f5f1ba2f28f6d4f4f4d4323a7
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2022-04-08 02:28:31 +0200

    learning resources app - done

--- src/components/learning-resourse/TheResource.vue
+++ src/components/learning-resourse/TheResource.vue
@@ -39,7 +39,9 @@ export default {
     provide(){
         return {
             resources : this.storedResources,
-            addResource : this.addResource
+            addResource : this.addResource,
+            deleteResource : this.removeResource
+
         }
     },
     computed: {
@@ -65,6 +67,12 @@ export default {
 
             this.storedResources.unshift(newResource);
             this.selectedTab = 'stored-resource'
+        }, 
+        removeResource(resId){
+            const resIndex = this.storedResources.findIndex(res => res.id === resId);
+            this.storedResources.splice(resIndex, 1);
+
+            console.log(this.storedResources)
         }
     }
 }
