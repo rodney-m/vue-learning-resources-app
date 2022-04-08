e304309b0c47187124b502a397075f5bdc551e1c
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2022-04-08 02:10:52 +0200

    error dialog - done

--- src/components/learning-resourse/TheResource.vue
+++ src/components/learning-resourse/TheResource.vue
@@ -3,8 +3,9 @@
         <base-button @click="setSelectedTab('stored-resource')" :mode="storedResButtonMode">Stored Resources</base-button>
         <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
     </base-card>
-
-    <component :is="selectedTab"></component>
+    <keep-alive>
+        <component :is="selectedTab"></component>
+    </keep-alive>
 </template>
 
 <script>
@@ -37,7 +38,8 @@ export default {
     },
     provide(){
         return {
-            resources : this.storedResources
+            resources : this.storedResources,
+            addResource : this.addResource
         }
     },
     computed: {
@@ -52,6 +54,17 @@ export default {
     methods: {
         setSelectedTab(tab){
             this.selectedTab = tab;
+        }, 
+        addResource(title, description, url){
+            const newResource = {
+                id : new Date().toISOString(),
+                title : title,
+                description : description,
+                link: url
+            };
+
+            this.storedResources.unshift(newResource);
+            this.selectedTab = 'stored-resource'
         }
     }
 }
